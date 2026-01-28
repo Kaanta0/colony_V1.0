2aa0b6dfb78b66d9b2d3064c7777e17d23de5333
Kaanta <107195318+Kaanta0@users.noreply.github.com>
2026-01-28 16:40:28 +0100

    Fix dummy detail view lifetime

--- src/sections.rs
+++ src/sections.rs
@@ -12,6 +12,12 @@ pub struct Section {
     pub filter: SectionFilter,
 }
 
+impl Section {
+    pub fn category(&self) -> Option<&AppCategory> {
+        self.filter.category.as_ref()
+    }
+}
+
 #[derive(Debug, Clone)]
 pub struct SectionFilter {
     origin: OriginFilter,
